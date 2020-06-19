f93775b8cafa5ba8b6d41adec1dbe55da0ef26ad
jonatas23 <jon23edw>
2020-06-19 13:57:29 -0300

    Segundo Commit

--- src/app/modules/administrador/administrador.module.ts
+++ src/app/modules/administrador/administrador.module.ts
@@ -1,19 +1,44 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import {CommonModule, LocationStrategy, PathLocationStrategy} from '@angular/common';
 
 import {AdministradorRoutingModule} from './administrador-routing.module';
 import {AdministradorComponent} from './administrador.component';
 import {HomeAdministradorComponent} from './home-administrador/home-administrador.component';
+import {SpinnerComponent} from './layouts/config/spinner.component';
+import {AppSidebarComponent} from './layouts/full/sidebar/sidebar.component';
+import {FullComponent} from './layouts/full/full.component';
+import {AppHeaderComponent} from './layouts/full/header/header.component';
+import {SharedModule} from './layouts/config/shared.module';
 import {DemoMaterialModule} from '../../demo-material-module';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {CdkTableModule} from '@angular/cdk/table';
+import {AuthGuard} from '../../service/auth/auth.guard';
+import {HttpClientModule} from '@angular/common/http';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
-  declarations: [AdministradorComponent, HomeAdministradorComponent],
+  declarations: [
+    AdministradorComponent,
+    HomeAdministradorComponent,
+    SpinnerComponent,
+    AppSidebarComponent,
+    FullComponent,
+    AppHeaderComponent
+  ],
   imports: [
     CommonModule,
     AdministradorRoutingModule,
     DemoMaterialModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    CdkTableModule,
+    FlexLayoutModule,
+    HttpClientModule,
+    DemoMaterialModule,
+    SharedModule
+  ],
+  providers: [AuthGuard,
+    {provide: LocationStrategy, useClass: PathLocationStrategy}
   ]
 })
 export class AdministradorModule {
