8674edce97fe34bb7959d475bc8a5244e05186d5
jonatas23 <jon23edw>
2020-06-19 14:12:15 -0300

    Segundo Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-  {path: '', loadChildren: './modules/public/public.module#PublicModule'},
-  {path: 'admin', loadChildren: './modules/administrador/administrador.module#AdministradorModule'},
-  {path: 'user', loadChildren: './modules/user/user.module#UserModule'},
-  {path: 'prof', loadChildren: './modules/profissional/profissional.module#ProfissionalModule'}
+  {path: '', loadChildren: () => import('./modules/public/public.module').then(m => m.PublicModule)},
+  {path: 'admin', loadChildren: () => import('./modules/administrador/administrador.module').then(m => m.AdministradorModule)},
+  {path: 'user', loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule)},
+  {path: 'prof', loadChildren: () => import('./modules/profissional/profissional.module').then(m => m.ProfissionalModule)}
 ];
 
 @NgModule({
