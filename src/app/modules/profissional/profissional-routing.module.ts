63ec46e6861bc48b10b26d436d704b36048488a3
jonatas23 <jon23edw>
2020-06-25 12:34:01 -0300

    Angular Modules Base

--- src/app/modules/profissional/profissional-routing.module.ts
+++ src/app/modules/profissional/profissional-routing.module.ts
@@ -2,13 +2,12 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ProfissionalComponent} from './profissional.component';
 import {HomeProfissionalComponent} from './home-profissional/home-profissional.component';
-
-const prefix:string = 'prof/';
+import {prefixProf} from '../../variable';
 
 const routes: Routes = [{
   path: '', component: ProfissionalComponent, children: [
-    {path: '', redirectTo: '/' + prefix + 'home', pathMatch: 'full'},
-    {path: prefix + 'home', component: HomeProfissionalComponent},
+    {path: '', redirectTo: '/' + prefixProf + 'home', pathMatch: 'full'},
+    {path: prefixProf + 'home', component: HomeProfissionalComponent},
   ]
 }];
 
